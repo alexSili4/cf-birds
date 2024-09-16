9ad6b583168c92b00421fa91322fbc591ba666f0
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-09-16 13:05:46 +0300

    Update onPaymentStart.js

--- src/js/main/onPaymentStart.js
+++ src/js/main/onPaymentStart.js
@@ -4,6 +4,7 @@ async function onPaymentStart(e) {
   const orderIdFieldName = 'order-id';
   const orderAmountFieldName = 'order-amount';
   const paymentDescFieldName = 'payment-description';
+  const branchIdFieldName = 'branch-id';
 
   const widgetData = e.currentTarget;
   const fetchIntentionStatusData = {};
@@ -11,6 +12,7 @@ async function onPaymentStart(e) {
   fetchIntentionStatusData[orderIdFieldName] = widgetData.attributes[orderIdFieldName].nodeValue;
   fetchIntentionStatusData[orderAmountFieldName] = widgetData.attributes[orderAmountFieldName].nodeValue;
   fetchIntentionStatusData[paymentDescFieldName] = widgetData.attributes[paymentDescFieldName].nodeValue;
+  fetchIntentionStatusData[branchIdFieldName] = widgetData.attributes[branchIdFieldName].nodeValue;
 
   fetchIntentionStatus(fetchIntentionStatusData);
 }
